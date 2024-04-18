c1786c7ad07475c1c4410de2c0aa2934b9182d95
AnggaD <angga.apta2022@gmail.com>
2024-04-18 17:27:37 +0700

    initial commit

---
+++ src/app/products/[productid]/reviews/[reviewid]/page.tsx
@@ -0,0 +1,11 @@
+export default function ReviewDetails({
+  params,
+}: {
+  params: { productid: string; reviewid: string };
+}) {
+  return (
+    <h1>
+      Review {params.reviewid} for product {params.productid}
+    </h1>
+  );
+}
